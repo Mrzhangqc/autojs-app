5cf07590893bed157d3baa9ea8da0e2f6495523a
qichang.zhang <qichang.zhang@mihoyo.com>
2022-05-16 17:59:42 +0800

    style: code style format

--- common/ExitApp.js
+++ common/ExitApp.js
@@ -3,18 +3,18 @@ var _uiUtils = function () {
   var TIMEOUT_FOR_LOOKUP_NODE = 250;
 
   // 获取当前应用的包名
-  var getCurrentPackage = function getPackageNameOfTheForegroundApplication(timeout) {
+  var getCurrentPackage = function (timeout) {
     var node = getRootNode(timeout);
     return node !== null ? node.packageName() : currentPackage();
   };
 
   // 获取 FrameLayout 根节点
-  var getRootNode = function getFrameLayoutNode(timeout) {
+  var getRootNode = function (timeout) {
     return className(ROOT_NODE_NAME).findOne(timeout || TIMEOUT_FOR_LOOKUP_NODE);
   };
 
   // 获取所有指定节点及其子节点的描述内容和文本内容
-  var getAllTextualContent = function getAllDescriptionAndTextUnderNodeRecursively(node) {
+  var getAllTextualContent = function (node) {
     var items = [];
     var getDescAndText = function(node) {
       if (node !== null) {
@@ -41,12 +41,12 @@ module.exports = (() => {
   var uiUtils = _uiUtils();
 
   // 通过模拟（多次）点击返回键退出当前应用，并显示当前应用的退出结果
-  var exitApp = function exitForegroundApplicationUsingReturnKey() {
+  var exitApp = function () {
     return showResult(returnUntilExitingApp());
   };
 
   // 显示当前应用的退出结果
-  var showResult = function showExitForegroundApplicationResult(result) {
+  var showResult = function (result) {
     var state = result.success ? 'has exited' : 'cannot exit';
     var message = getAppName(result.package) + ' ' + state;
     toast(message);
@@ -54,7 +54,7 @@ module.exports = (() => {
   };
 
   // 一直按下返回键，直至当前应用退出为止
-  var returnUntilExitingApp = function keepReturningUntilExitingForegroundApplication() {
+  var returnUntilExitingApp = function () {
     var TIME_REQUIRED_FOR_RETURN = 100;
     var MAXIMUM_RETURN_IN_SAME_UI = 10;
     var prevPackage = uiUtils.getCurrentPackage();
@@ -79,7 +79,7 @@ module.exports = (() => {
   };
 
   // 获取相同界面的次数
-  var getSameUiInfo = function getInfoAboutNumberOfTimesOfTheSameUi() {
+  var getSameUiInfo = function () {
     return {
       number: 1,
       prevText: null,
